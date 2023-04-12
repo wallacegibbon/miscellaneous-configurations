4563f54aae2b37764b571574890d75de8c407b0f
wallace <wallacegibbon@aliyun.com>
2023-04-12 13:00:32 +0800

    Improvements: A useful script (who works like the `realpath` command) is added. And some indentation changes.

--- qemu-misc.sh
+++ qemu-misc.sh
@@ -7,22 +7,22 @@ qemu-img create -f qcow2 kylin-v10-aarch64.qcow2 50G
 ## Install OS from ISO
 
 qemu-system-aarch64 \
-  -m 4096 \
-  -cpu cortex-a72 -smp 2,cores=2,threads=1,sockets=1 -M virt \
-  -bios ./QEMU_EFI.fd \
-  -device VGA -device nec-usb-xhci -device usb-mouse -device usb-kbd \
-  -net nic -net tap \
-  -cdrom /home/wallace/Downloads/Kylin-Server-10-SP2-Release-Build09-20210524-arm64.iso \
-  -boot d ./kylin-v10-aarch64.qcow2
+-m 4096 \
+-cpu cortex-a72 -smp 2,cores=2,threads=1,sockets=1 -M virt \
+-bios ./QEMU_EFI.fd \
+-device VGA -device nec-usb-xhci -device usb-mouse -device usb-kbd \
+-net nic -net tap \
+-cdrom /home/wallace/Downloads/Kylin-Server-10-SP2-Release-Build09-20210524-arm64.iso \
+-boot d ./kylin-v10-aarch64.qcow2
 
 
 ## Start the machine
 
 qemu-system-aarch64 \
-  -m 4096 \
-  -cpu cortex-a72 -smp 2,cores=2,threads=1,sockets=1 -M virt \
-  -bios ./QEMU_EFI.fd \
-  -device VGA -device nec-usb-xhci -device usb-mouse -device usb-kbd \
-  -net nic -net tap \
-  -boot d ./kylin-v10-aarch64.qcow2
+-m 4096 \
+-cpu cortex-a72 -smp 2,cores=2,threads=1,sockets=1 -M virt \
+-bios ./QEMU_EFI.fd \
+-device VGA -device nec-usb-xhci -device usb-mouse -device usb-kbd \
+-net nic -net tap \
+-boot d ./kylin-v10-aarch64.qcow2
 
---
+++ shscripts/abs_filename.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+## this works like the `realpath` command.
+
+abs_filename() {
+	echo $(cd "$(dirname "$1")" && pwd)/$(basename "$1")
+}
+
+abs_filename $1
