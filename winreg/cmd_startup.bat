f3ab55ca70259a0f8da3f6408de536ab0b651939
wallacegibbon <wallacegibbon@aliyun.com>
2024-05-09 10:14:50 +0800

    The `cmd_startup.bat` script may cause weird bugs for some command line tools like the SDCC compiler.

--- winreg/cmd_startup.bat
+++ winreg/cmd_startup.bat
@@ -1,3 +1,6 @@
+REM It's not recommended to use this script, weird things may happend.
+REM e.g. If you do some printing in init.cmd, commands like SDCC compiler, who depends on CMD output, may fail.
+
 reg add "HKCU\Software\Microsoft\Command Processor" /v AutoRun /t REG_EXPAND_SZ /d %USERPROFILE%\init.cmd /f
 
 REM reg delete "HKCU\Software\Microsoft\Command Processor" /v AutoRun
