baf5fbb9eb51c687e3db57fffd7bcaf6cb9c0505
wallace <wallacegibbon@aliyun.com>
2024-08-01 14:03:09 +0800

    New File: The bash script `.bash-config.sh` for changing the `PS1` got introduced.

---
+++ .bash-config.sh
@@ -0,0 +1,5 @@
+## Put this file in $HOME and add the following line to `$HOME/.bash_profile` or `$HOME/.bashrc`.
+## . ~/.bash-config.sh
+
+PS1="\[\e[1;4;34m\]$(uname -sr)\[\e[0m\][\[\e[33m\]\t\[\e[0m\]] \[\e[35m\]\w\[\e[0m\]\$ "
+
