b0801957492483d09c951be9f40be97255dba6be
wallace <wallacegibbon@aliyun.com>
2024-11-09 23:51:02 +0800

    File `.bash-config.sh` has changed. Better `PS1` setting to support more platforms. (`uname -sr` get weird string on Milk-v Duo)

--- .bash-config.sh
+++ .bash-config.sh
@@ -1,5 +1,5 @@
 ## Put this file in $HOME and add the following line to `$HOME/.bash_profile` or `$HOME/.bashrc`.
 ## . ~/.bash-config.sh
 
-PS1="\[\e[1;4;34m\]$(uname -sr)\[\e[0m\][\[\e[33m\]\t\[\e[0m\]] \[\e[35m\]\w\[\e[0m\]\$ "
+PS1="\[\e[1;4;34m\]$(uname -o) $(uname -m)\[\e[0m\][\[\e[33m\]\t\[\e[0m\]] \[\e[35m\]\w\[\e[0m\]\$ "
 
