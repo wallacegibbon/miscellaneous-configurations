d2075cc30f721efacd761663cd56b97051c9136b
wallace <wallacegibbon@aliyun.com>
2024-12-21 12:49:42 +0800

    Bash Configuration Update and Vim Configuration Change: Default tab size is 2-spaces now since Elixir is the main tool for now.

--- .bash-config.sh
+++ .bash-config.sh
@@ -4,3 +4,9 @@
 OS_MACHINE_TYPE=$(uname -sm | sed -n 's/^\([A-Za-z0-9_]*\).* \(.*\)/\1 \2/p')
 PS1="\[\e[1;4;34m\]${OS_MACHINE_TYPE}\[\e[0m\][\[\e[33m\]\t\[\e[0m\]] \[\e[35m\]\w\[\e[0m\]\$ "
 
+ELIXIR_HOME="$HOME/elixir-1.18.0-otp-26"
+
+case ":$PATH:" in
+  *:"$ELIXIR_HOME/bin":*) ;;
+  *) export PATH="$ELIXIR_HOME/bin:$PATH";;
+esac
