29dbc4c5245e308d111079ab2051d016aec60b67
wallace <wallacegibbon@aliyun.com>
2024-12-21 22:05:39 +0800

    Simplification: The PATH configuration `.bash-config.sh` is not very useful and got removed to support more platforms easier.

--- .bash-config.sh
+++ .bash-config.sh
@@ -1,11 +1,5 @@
-## Put this file in $HOME and add the following line to `$HOME/.bash_profile` or `$HOME/.bashrc`.
+## Copy this file to $HOME. In `$HOME/.bash_profile` or `$HOME/.bashrc`, add the following command:
 ## . ~/.bash-config.sh
 
 OS_MACHINE_TYPE=$(uname -sm | sed -n 's/^\([A-Za-z0-9_]*\).* \(.*\)/\1 \2/p')
 PS1="\[\e[1;4;34m\]${OS_MACHINE_TYPE}\[\e[0m\][\[\e[33m\]\t\[\e[0m\]] \[\e[35m\]\w\[\e[0m\]\$ "
-
-LOCAL_BIN=$HOME/.local/bin
-case ":$PATH:" in
-*:"$LOCAL_BIN":*) ;;
-*               ) export PATH="$LOCAL_BIN:$PATH";;
-esac
