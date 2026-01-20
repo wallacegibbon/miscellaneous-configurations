5623537bc3aa853d9fd9f548b7f4fcfa1643c604
Wallace Gibbon <wallacegibbon@aliyun.com>
2026-01-20 09:56:01 +0800

    Script to configure Gnome through `gsettings`. (keyboard shortcut, etc.)

---
+++ gnome-prepare.sh
@@ -0,0 +1,5 @@
+gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Control>backslash']"
+gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward "['<Control><Shift>backslash']"
+gsettings set org.gnome.desktop.wm.keybindings switch-input-source "['<Control>space']"
+gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward "['<Control><Shift>space']"
+gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/ scrollbar-policy 'never'
