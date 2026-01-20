22f16c6195940320cf3a89b6a533f3b299973050
Wallace Gibbon <wallacegibbon@aliyun.com>
2026-01-20 10:10:13 +0800

    Feature: The wireguard script and systemd file support restarting now.

--- init-wireguard.sh
+++ init-wireguard.sh
@@ -2,11 +2,18 @@
 
 set -e
 
-ip link add dev wg0 type wireguard
-ip link set up wg0
+if test "$#" -ne 1 || test -z "$1"; then
+	echo "Usage example: $0 wg0" >&2
+	exit 1
+fi
+
+IFACE_NAME=$1
+
+ip link add dev $IFACE_NAME type wireguard
+ip link set up $IFACE_NAME
 
 ## Change the ip address.
-ip addr add 172.16.9.6/24 dev wg0
+ip addr add 172.16.9.6/24 dev $IFACE_NAME
 
 ## Change the file `wg.XXX.private`.
-wg set wg0 private-key /home/wallace/wg.XXX.private peer $(cat /home/wallace/wg.xinrenfei.wang.public) endpoint xinrenfei.wang:32768 allowed-ips 0.0.0.0/0 persistent-keepalive 25
+wg set $IFACE_NAME private-key /home/wallace/wg.XXX.private peer $(cat /home/wallace/wg.xinrenfei.wang.public) endpoint xinrenfei.wang:32768 allowed-ips 0.0.0.0/0 persistent-keepalive 25
